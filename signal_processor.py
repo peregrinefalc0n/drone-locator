8374b20ae93ab62ceee109e0b244909077a1586b
Robert Matsar <robert.matsar@gmail.com>
2024-03-26 14:13:28 +0200

    made signal processor look at all signals in sampled range while ignoring DC spike

--- signal_processor.py
+++ signal_processor.py
@@ -1,6 +1,6 @@
 from pyhackrf2 import HackRF
 import numpy as np
-from matplotlib.pyplot import psd
+from matplotlib.pyplot import psd, xlabel, ylabel, show
 class Signal:
     """Class that represents a signal"""
     def __init__(self, start_freq, end_freq, peak_power_db, peak_freq):
@@ -31,7 +31,12 @@ class SignalProcessor:
     
     def __process(self, offset = 10) -> list:
         """Processes the samples and returns a list of signals that are above the noise floor by the given offset in dBm"""
-        pxx, freqs = psd(self.__measure(), NFFT=2048, Fs=self.hackrf.sample_rate/1e6, Fc=self.hackrf.center_freq/1e6)
+        pxx, freqs, line = psd(self.__measure(), NFFT=2048, Fs=self.hackrf.sample_rate/1e6, Fc=self.hackrf.center_freq/1e6, return_line=True)
+
+        #display graph
+        xlabel('Frequency (MHz)')
+        ylabel('Relative power (dB)')
+        show()
 
         #Take the first 1000 samples to avoid the DC spike (essentially taking less than half of our samples (we had 2048 of them))
         select_count = 1000
@@ -41,17 +46,26 @@ class SignalProcessor:
         #Find the minimum power in the sample to see where the noise floor is
         min_index = np.argmin(pxx_sample)
         min_pxx_db = self.__mW_to_dBm(pxx_sample[min_index])
-
+        
         #TODO find a more elegant or dynamic solution instead of hardcoding the offset
+        
+        #reset the sample arrays to max size again
+        freqs_sample = freqs
+        pxx_sample = pxx
         
         #Find the signals that are above the noise floor by the given offset
         signals_list = list()
         index = 0
         while index < len(pxx_sample):
+            #skip DC spike
+            if index == 1000:
+                index = 1048
             power_db = self.__mW_to_dBm(pxx_sample[index])
             if power_db >= min_pxx_db + offset:
                 temp_signal = Signal(freqs_sample[index], freqs_sample[index], power_db, freqs_sample[index])
-                while power_db >= min_pxx_db + offset:
+                power_db = self.__mW_to_dBm(pxx_sample[index])
+                while (power_db >= min_pxx_db + offset) and (index < len(pxx_sample)):
+                    power_db = self.__mW_to_dBm(pxx_sample[index])
                     temp_signal.end_freq = freqs_sample[index]
                     if power_db > temp_signal.peak_power_db:
                         temp_signal.peak_power_db = power_db
@@ -69,4 +83,4 @@ class SignalProcessor:
 if __name__ == "__main__":
     signal_processor = SignalProcessor(id=1)
     for signal in signal_processor.get_signals():
-        print("Signal from", signal.start_freq, "to", signal.end_freq, "with peak power of", signal.peak_power_db)
+        print("Signal from", signal.start_freq, "to", signal.end_freq, "with peak power of", signal.peak_power_db, " at freq ", signal.peak_freq)
